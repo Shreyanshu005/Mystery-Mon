f75990a91cedd2bf675f1fc78cf2aa33481277e5
Shreyanshu005 <130226682+Shreyanshu005@users.noreply.github.com>
2024-09-14 10:11:44 +0530

    Add files via upload

--- pokemon.js
+++ pokemon.js
@@ -4,13 +4,15 @@ var id = Math.floor((Math.random() * pokemonCount) + 1);
 var naam;
 var guessTimer;
 var timerInterval;
-var isPaused = false; // Variable to track if the timer is paused
-
-window.onload = async function() {
+var isPaused = false;
+// var playerNameInput = document.getElementById('player-name');
+// var playerName = '';
+// playerName = playerNameInput.value.trim();
+window.onload = async function () {
     showLoadingScreen();
     await getPokemon(id);
     console.log(pokedex);
-    startGuessTimer(); // Start the timer after loading the Pokémon
+    startGuessTimer();
 }
 
 async function getPokemon(num) {
@@ -23,6 +25,7 @@ async function getPokemon(num) {
     naam = pokemonName;
     let pokemonType = pokemon["types"].map(typeInfo => typeInfo.type.name).join(', ');
     let pokemonImage = pokemon["sprites"]["front_default"];
+    
     res = await fetch(pokemon["species"]["url"]);
     let pokemonDes = await res.json();
     pokemonDes = pokemonDes["flavor_text_entries"][0]["flavor_text"];
@@ -35,14 +38,14 @@ async function getPokemon(num) {
     };
 
     const imgElement = document.getElementById('pokemon-image');
-    imgElement.style.display = 'none'; // Hide the image initially
+    imgElement.style.display = 'none';
     imgElement.src = pokemonImage;
-    imgElement.onload = function() {
+    imgElement.onload = function () {
         hideLoadingScreen();
-        imgElement.style.display = 'block'; // Show the image once it has loaded
-        document.querySelector('.reveal').disabled = false; // Enable the reveal button
+        imgElement.style.display = 'block';
+        document.querySelector('.reveal').disabled = false;
     };
-   
+
 }
 
 function showLoadingScreen() {
@@ -57,33 +60,33 @@ function startGuessTimer() {
     let timeLeft = 10;
     document.getElementById('timer-clock').innerText = timeLeft;
     const progressBar = document.getElementById('progress-bar');
-    progressBar.style.width = '0%'; // Reset progress bar
+    progressBar.style.width = '0%';
 
-    let interval = 100; // Update every 100 milliseconds
-    let totalTime = timeLeft * 1000; // Total time in milliseconds
+    let interval = 100;
+    let totalTime = timeLeft * 1000;
     let elapsedTime = 0;
 
-    timerInterval = setInterval(function() {
-        if (!isPaused) { // Check if the timer is not paused
+    timerInterval = setInterval(function () {
+        if (!isPaused) {
             elapsedTime += interval;
             let percentage = (elapsedTime / totalTime) * 100;
-            progressBar.style.width = `${percentage}%`; // Update progress bar
+            progressBar.style.width = `${percentage}%`;
 
             if (elapsedTime >= totalTime) {
                 clearInterval(timerInterval);
                 revealPokemon();
-                document.querySelector('.progress-bar-container').style.display = 'none'; 
+                document.querySelector('.progress-bar-container').style.display = 'none';
                 document.querySelector('.reveal').style.display = 'none';
                 document.getElementById('who').style.display = 'none';
                 document.querySelector('.boxImg').style.marginBottom = '70px';
                 document.querySelector('.link').style.display = 'flex';
                 const linkElement = document.getElementById('linkto');
                 linkElement.style.display = 'block';
-                linkElement.href = `https://pokepedia-graphql.netlify.app/pokemon/`+naam;
-                
+                linkElement.href = `https://pokepedia-graphql.netlify.app/pokemon/` + naam;
+
+                document.getElementById('welcome').style.display='none';
             }
 
-            // Update the timer display every second
             if (elapsedTime % 1000 === 0) {
                 timeLeft--;
                 document.getElementById('timer-clock').innerText = timeLeft;
@@ -94,17 +97,15 @@ function startGuessTimer() {
 
 function revealPokemon() {
     const imgElement = document.getElementById('pokemon-image');
-    imgElement.style.display = 'block'; // Ensure the image is displayed
-    imgElement.classList.add('brightened'); 
+    imgElement.style.display = 'block';
+    imgElement.classList.add('brightened');
 
-    // Add the up-down class when the brightness is set to 1
     if (imgElement.classList.contains('brightened')) {
         imgElement.classList.add('up-down');
     }
 
     document.getElementById('correct').innerText = 'The Pokémon is ' + naam;
 
-    // Add border class to info div
     const infoDiv = document.querySelector('.info');
     if (pokedex[id].types && pokedex[id].description) {
         infoDiv.classList.add('has-content');
@@ -112,29 +113,29 @@ function revealPokemon() {
         typeText(document.getElementById('pokemon-description'), 'Description : ' + pokedex[id].description);
     }
 
-    // Disable and hide the input field
     const inputField = document.getElementById('guessPokemon');
     inputField.disabled = true;
     inputField.style.display = 'none';
 }
 
 function typeText(element, text) {
-    element.innerHTML = ''; // Clear existing content
-    element.classList.remove('typing'); // Remove the typing class to reset the animation
+    element.innerHTML = '';
+    element.classList.remove('typing');
     setTimeout(() => {
-        element.innerHTML = text; // Set the text content
-        element.classList.add('typing'); // Add the typing class to start the animation
-    }, 100); // Small delay to ensure the animation restarts
+        element.innerHTML = text;
+        element.classList.add('typing');
+    }, 100);
 }
 
+
 const inputField = document.getElementById('guessPokemon');
-inputField.addEventListener('keydown', function(event) {
+inputField.addEventListener('keydown', function (event) {
     if (event.key === 'Enter') {
-        const userInput = inputField.value.trim().toLowerCase(); // Get user input and normalize it
+        const userInput = inputField.value.trim().toLowerCase();
         if (userInput === naam.toLowerCase()) {
-            clearInterval(timerInterval); // Stop the timer
+            clearInterval(timerInterval);
             const imgElement = document.getElementById('pokemon-image');
-            imgElement.style.display = 'block'; // Ensure the image is displayed
+            imgElement.style.display = 'block';
             imgElement.classList.add('brightened');
             document.getElementById('correct').innerText = 'You nailed it ! The Pokémon is ' + naam;
             document.getElementById('who').style.display = 'none';
@@ -142,9 +143,9 @@ inputField.addEventListener('keydown', function(event) {
             document.querySelector('.link').style.display = 'flex';
             const linkElement = document.getElementById('linkto');
             linkElement.style.display = 'block';
-            linkElement.href = `https://pokepedia-graphql.netlify.app/pokemon/`+naam;
+            linkElement.href = `https://pokepedia-graphql.netlify.app/pokemon/` + naam;
 
-            // Add border class to info div
+            document.getElementById('welcome').style.display='none';
             const infoDiv = document.querySelector('.info');
             if (pokedex[id].types && pokedex[id].description) {
                 infoDiv.classList.add('has-content');
@@ -152,10 +153,9 @@ inputField.addEventListener('keydown', function(event) {
                 typeText(document.getElementById('pokemon-description'), 'Description: ' + pokedex[id].description);
             }
 
-            // Disable and hide the input field
             inputField.disabled = true;
             inputField.style.display = 'none';
-            document.querySelector('.progress-bar-container').style.display = 'none'; // Hide progress bar
+            document.querySelector('.progress-bar-container').style.display = 'none';
             document.querySelector('.reveal').style.display = 'none';
         } else {
             document.getElementById('correct').innerText = 'Oops! Try Again';
@@ -163,34 +163,33 @@ inputField.addEventListener('keydown', function(event) {
     }
 });
 
-document.querySelector('.again').addEventListener('click', async function() {
-    clearInterval(timerInterval); // Clear the previous timer
-    location.reload(); 
+document.querySelector('.again').addEventListener('click', async function () {
+    clearInterval(timerInterval);
+    location.reload();
 
-    // Remove border class from info div
     const infoDiv = document.querySelector('.info');
     infoDiv.classList.remove('has-content');
-    document.querySelector('.reveal').disabled = true; // Disable the reveal button
+    document.querySelector('.reveal').disabled = true;
 
-    // Enable and show the input field
     inputField.disabled = false;
     inputField.style.display = 'block';
 });
 
-document.querySelector('.reveal').addEventListener('click', function() {
-    clearInterval(timerInterval); // Clear the timer if the user clicks reveal
+document.querySelector('.reveal').addEventListener('click', function () {
+    clearInterval(timerInterval);
     revealPokemon();
-    document.querySelector('.progress-bar-container').style.display = 'none'; // Hide progress bar
+    document.querySelector('.progress-bar-container').style.display = 'none';
     document.querySelector('.reveal').style.display = 'none';
     document.getElementById('who').style.display = 'none';
     document.querySelector('.boxImg').style.marginBottom = '70px';
     document.querySelector('.link').style.display = 'flex';
+    document.getElementById('welcome').style.display='none';
     const linkElement = document.getElementById('linkto');
     linkElement.style.display = 'block';
-    linkElement.href = `https://pokepedia-graphql.netlify.app/pokemon/`+naam;
+    linkElement.href = `https://pokepedia-graphql.netlify.app/pokemon/` + naam;
 });
 
-document.querySelector('.pause').addEventListener('click', function() {
-    isPaused = !isPaused; // Toggle the pause state
-    this.innerText = isPaused ? 'Resume' : 'Pause'; // Update button text
+document.querySelector('.pause').addEventListener('click', function () {
+    isPaused = !isPaused;
+    this.innerText = isPaused ? 'Resume' : 'Pause';
 });
